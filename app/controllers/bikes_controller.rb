f88a3e78f41f73899031588f226f3abd118a7120
Paul Lanza <paullanza@gmail.com>
2022-05-17 14:05:59 -0400

    Edit bikes action and view complete

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,5 +1,5 @@
 class BikesController < ApplicationController
-  before_action :find_bike, only: [:show]
+  before_action :find_bike, only: [:show, :edit, :update]
   def index
     @bikes = Bike.all
   end
@@ -7,9 +7,24 @@ class BikesController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @bike.update(bike_params)
+      redirect_to @bike, notice: 'Your updated bike is ready.'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def find_bike
     @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:brand, :category, :location, :price_per_day, :description)
   end
 end
