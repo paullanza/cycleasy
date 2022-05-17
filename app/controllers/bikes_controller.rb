b38d5abf7fc8fdfa40b47e35b623f3cba64c7296
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-17 13:59:00 -0400

    finished the destroy action

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,10 +1,16 @@
 class BikesController < ApplicationController
-  before_action :find_bike, only: [:show]
+  before_action :find_bike, only: [:show, :destroy]
   def index
     @bikes = Bike.all
   end
 
   def show
+  end
+
+  def destroy
+    @bike.destroy if @bike.user == current_user
+
+    redirect_to bikes_path
   end
 
   private
