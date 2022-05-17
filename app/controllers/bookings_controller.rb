5bba4be1d379d822b6a76eff71cb0be0982cec4f
sharon alexa acevedo <alexa_acevedo2@hotmail.com>
2022-05-17 17:55:17 -0400

    show view of booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,16 @@
 class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show]
+
   def my_bookings
     @bookings = Booking.where(user: current_user)
+  end
 
+  def show
+  end
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:id])
   end
 end
