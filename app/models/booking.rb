cb85260ae01c2529b1e6739f62b9a1832811c7de
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-17 15:39:59 -0400

    did the enum in the model booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,11 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :bike
   validates :user, :bike, :booking_end, :booking_start, :total_price, :status, presence: true
+
+  enum status: {
+    pending: 0,
+    accepted: 1,
+    declined: 2,
+    canceled: 3
+  }
 end
