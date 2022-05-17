858240ac4903005f2708e7c7b1657562fb38f3d1
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-17 17:43:35 -0400

    implemet the create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
-  before_action :find_bike, only: [:create]
+  before_action :find_bike, only: [:create, :new]
+
   def my_bookings
     @bookings = Booking.where(user: current_user)
   end
@@ -9,17 +10,25 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = booking.new
-    @booking.
+    @booking = Booking.new(booking_params)
+    @booking.bike = @bike
+    @booking.user = current_user
+    @booking.total_price = (@booking.booking_end - @booking.booking_start) * @bike.price_per_day
+
+    if @booking.save
+      redirect_to my_booking_path(@booking)
+    else
+      render 'bikes/show'
+    end
   end
 
   private
 
   def find_bike
-    @bike = Bike.find(params[:id])
+    @bike = Bike.find(params[:bike_id])
   end
 
-  def bike_params
-    params.require(:bike).permit(:booking_start, :booking_end, :total_price)
+  def booking_params
+    params.require(:booking).permit(:booking_start, :booking_end, :total_price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   get '/bikes/my_bikes', to: 'bikes#my_bikes', as: :my_bikes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bikes
+  resources :bikes do
+    post '/bookings', to: 'bookings#create'
+  end
 end
