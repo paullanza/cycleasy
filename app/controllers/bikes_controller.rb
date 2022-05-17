8d89ed2d0fd6b9adbe42e4456adbd97578ec77b3
sharon alexa acevedo <alexa_acevedo2@hotmail.com>
2022-05-17 14:27:51 -0400

    new create on controller + the view

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -7,9 +7,27 @@ class BikesController < ApplicationController
   def show
   end
 
+  # Sharon : method New
+  def new
+    @bike = Bike.new
+  end
+
+  # Sharon : method Create
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.user = current_user
+    @bike.save
+    # redirect to owner bike list
+    redirect_to bike_path(@bike)
+  end
+
   private
 
   def find_bike
     @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:brand, :category, :location, :price_per_day, :description)
   end
 end
