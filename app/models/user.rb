063b1fce2d5a76a0c7960614faa58df1fdd81a54
Paul Lanza <paullanza@gmail.com>
2022-05-17 16:52:59 -0400

    Added destruction dependancies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
-  has_many :bikes
-  has_many :bookings
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+  has_many :bikes, dependent: :delete_all
+  has_many :bookings, dependent: :delete_all
 end
