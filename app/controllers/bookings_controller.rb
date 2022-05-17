cb9eee98368c31b95b9f0fb42e6ed137d8732ac6
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-17 17:00:06 -0400

    working on creating a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,25 @@
 class BookingsController < ApplicationController
+  before_action :find_bike, only: [:create]
   def my_bookings
     @bookings = Booking.where(user: current_user)
+  end
 
+  def new
+    @bookings = Booking.new
+  end
+
+  def create
+    @booking = booking.new
+    @booking.
+  end
+
+  private
+
+  def find_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(:booking_start, :booking_end, :total_price)
   end
 end
