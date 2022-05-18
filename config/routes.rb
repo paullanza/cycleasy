db03440a3505eb8ce7a3efe9fef070622d3047de
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-18 13:39:09 -0400

    can now accept or decline a booking

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/bookings/my_bookings', to: 'bookings#my_bookings', as: :my_booking
+  get '/bookings/:id/accepted', to: 'bookings#accepted', as: :accept_booking
+  get '/bookings/:id/declined', to: 'bookings#declined', as: :decline_booking
 
   get '/bikes/my_bikes', to: 'bikes#my_bikes', as: :my_bikes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
