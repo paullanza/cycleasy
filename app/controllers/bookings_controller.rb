eed9123cb7b4cbd9c69490fb0c2d5f5d9912c166
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-05-18 13:59:37 -0400

    updated the seeds

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
 
   def my_bookings
     @bookings = Booking.where(user: current_user)
-    @my_bikes = Booking.where(bike: current_user)
+    @bikes_bookings = Booking.where(bike: current_user)
   end
 
   def new
